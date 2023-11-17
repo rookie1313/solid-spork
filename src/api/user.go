57e62abe8dd8a9ff002eec13e4d244b5cf0becd3
pepno <yizheliu035@gmail.com>
2023-11-17 08:55:19 +0800

    add universal error handler and createUser validation for httpRequest

--- src/api/user.go
+++ src/api/user.go
@@ -13,14 +13,22 @@ func (server *Server) UserRoute(router fiber.Router) {
 func (server *Server) createUser(ctx *fiber.Ctx) error {
 	req := new(dtos.CreateUserRequest)
 	if err := ctx.BodyParser(req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(err)
+		return &fiber.Error{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		}
 	}
-
+	if err := validate.Struct(req); err != nil {
+		return &fiber.Error{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		}
+	}
 	e := &model.User{Email: req.Email, Password: req.Password}
 	result := server.DB.Create(e)
 	if result.Error != nil {
 		return result.Error
 	}
 
-	return nil
+	return ctx.Status(fiber.StatusCreated).JSON(e)
 }
