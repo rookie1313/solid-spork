b114b15f4b773772e34a609f0605cecc4c6b4309
rookie1313 <lyzxilocent@163.com>
2023-11-20 13:54:17 +0800

    add get userByEmail (#4)
    Co-authored-by: pepno <yizheliu035@gmail.com>

--- src/dtos/user_request.go
+++ src/dtos/user_request.go
@@ -2,5 +2,6 @@ package dtos
 
 type CreateUserRequest struct {
 	Email    string `json:"email" validate:"required,email"`
+	Name     string `json:"name" validate:"required"`
 	Password string `json:"password" validate:"required,min=6"`
 }
